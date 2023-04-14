6c3bbf3009e6500dbcfba14020f76c31a6db4576
1deyce <124916049+1deyce@users.noreply.github.com>
2023-04-14 11:56:04 +0200

    Update3(hero right)

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -77,28 +77,69 @@ const Hero = () => {
                     </span>
                     <span>
                         <a 
-                            href="#linkedin" 
+                            href="https://www.instagram.com/1deycefr/" 
                             className='text-smallTextColor text-[15px] font-[600]'
                         >
-                            <i class="ri-linkedin-line"></i>
-                        </a>
-                    </span>
-                    <span>
-                        <a 
-                            href="#linkedin" 
-                            className='text-smallTextColor text-[15px] font-[600]'
-                        >
-                            <i class="ri-linkedin-line"></i>
+                            <i class="ri-instagram-line"></i>
                         </a>
                     </span>
                 </div>
 
                     </div>
                     {/* ========== hero left end ========== */}
+                    {/* ========== hero img ========== */}
+                    <div className='basis-1/3 mt-10 sm:mt-0'>
+                        <figure className='flex items-center justify-center'>
+                            <img src={heroImg} alt="" />
+                        </figure>
+
+                    </div>
+                    {/* ========== hero img end ========== */}
+                    {/* ========== hero content right ========== */}
+                    <div className='md:basis-1/5 flex justify-between text-center mt-10 flex-wrap gap-3 md:mt-0 md:flex-col md:justify-end md:text-end'>
+                        <div className='mb-10'>
+                            <h2 className='text-headingColor font-[700] text-[32px]'>
+                                <CountUp start={0} end={1} duration={1} suffix='+' />
+                            </h2>
+                            <h4 className='text-headingColor font-[600] text-[18px]'
+                            >
+                                Years of experience
+                            </h4>
+                        </div>
+                        <div className='mb-10'>
+                            <h2 className='text-headingColor font-[700] text-[32px]'>
+                                <CountUp start={0} end={100} duration={2} suffix='%' />
+                            </h2>
+                            <h4 className='text-headingColor font-[600] text-[18px]'
+                            >
+                                Success rate
+                            </h4>
+                        </div>
+                        <div className='mb-10'>
+                            <h2 className='text-headingColor font-[700] text-[32px]'>
+                                <CountUp start={0} end={10} duration={3} suffix='+' />
+                            </h2>
+                            <h4 className='text-headingColor font-[600] text-[18px]'
+                            >
+                                Happy clients
+                            </h4>
+                        </div>
+                        <div className='mb-10'>
+                            <h2 className='text-headingColor font-[700] text-[32px]'>
+                                <CountUp start={0} end={10} duration={4} suffix='+' />
+                            </h2>
+                            <h4 className='text-headingColor font-[600] text-[18px]'
+                            >
+                                Projects completed
+                            </h4>
+                        </div>
+
+                    </div>
+                    {/* ========== hero content right end ========== */}
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Hero;
