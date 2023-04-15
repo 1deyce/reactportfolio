5e318bd2c286b8a8ae282cb40d1531af86c02656
1deyce <124916049+1deyce@users.noreply.github.com>
2023-04-15 13:38:14 +0200

    Update5
    Portfolio.jsx begun

--- src/components/UI/Portfolio.jsx
+++ src/components/UI/Portfolio.jsx
@@ -3,7 +3,27 @@ import React from "react";
 const Portfolio = () => {
     return <section id="portfolio">
         <div className="container">
+            <div className="flex items-center justify-between flex-wrap">
+                <div className="mb-7 sm:mb-0">
+                    <h3 className="text-headingColor text-[2rem] font-[700]">
+                        My Recent Projects
+                    </h3>
+                </div>
 
+            <div className="flex gap-3">
+                <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
+                    All
+                </button>
+                <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
+                    All
+                </button>
+                <button className="text-smallTextColor border border-solid border-smallTextColor py-2 px-4 rounded-[8px]">
+                    All
+                </button>
+
+            </div>
+
+            </div>
         </div>
     </section>
 };
