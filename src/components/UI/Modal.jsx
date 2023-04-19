d468b69528fa88149f8b23133acaeda9fa5a231c
1deyce <124916049+1deyce@users.noreply.github.com>
2023-04-19 15:47:58 +0200

    Update_Modal

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -17,7 +17,36 @@ const Modal = ({activeID, setShowModal}) => {
                     <h2 className="text-2xl text-headingColor font-[700] my-5">
                         {portfolio.title}
                     </h2>
+
+                    <p className="text-[15px] leading-7 text-smallTextColor">
+                        {portfolio.description}
+                    </p>
+
+                    <div className="mt-5 flex items-center gap-3 flex-wrap">
+                        <h4 className="text-headingColor text-[18px] text-[700]">
+                            Technologies:
+                        </h4>
+
+                        {portfolio.technologies.map((item,index)=> (
+                            <span 
+                                key={index} 
+                                className="bg-gray-200 py-1 px-2 rounded-[5px] text-[14px] leading-0"
+                            >
+                                {item}
+                            </span>
+                        ))}
+                    </div>
+
+                <a href={portfolio.siteUrl}>
+                    <button className="bg-primaryColor text-white py-2 px-4 my-8 rounded-[8px] font-[500] hover:bg-headingColor ease-in duration-300">
+                        Live Site
+                    </button>
+                </a>
                 </div>
+
+        <button onClick={()=>setShowModal(false)} className="w-[1.8rem] h-[1.8rem] bg-[white] absolute top-[1.7rem] right-[1.7rem] text-[25px] flex items-center justify-center rounded-[3px] leading-0 cursor-pointer">&times;</button>
+                
+
             </div>
         </div>
     );
