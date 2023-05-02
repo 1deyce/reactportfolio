6e2281e2836d834247dd728839e2f98191127088
1deyce <124916049+1deyce@users.noreply.github.com>
2023-05-02 15:51:46 +0200

    Update Header.jsx
    removed sticky navbar, added onclicks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,20 @@
-import React, { useRef, useEffect } from 'react';
 
 const Header = () => {
 
-    const headerRef = useRef(null)
+    const handleClick = e=>{
+        e.preventDefault()
 
-    const stickyHeaderFunc = ()=>{
-        window.addEventListener('scroll', ()=>{
-            if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
-                headerRef.current.classList.add('sticky__header')
-            }else{
-                headerRef.current.classList.remove('sticky__header')
-            }
-        })
-    }
+        const targetAttr = e.target.getAttribute('href')
+        const location = document.querySelector(targetAttr).offsetTop
 
-    useEffect(()=>{
-        stickyHeaderFunc()
-
-        return window.removeEventListener('scroll', stickyHeaderFunc)
-    },[])
+        window.scrollTo({
+            top: location - 80,
+            left: 0,
+        });
+    };
 
     return (
     <header 
-        ref={headerRef} 
         className='w-full h-[80px] leading-[80px] flex items-center bg-[#12141e]'
     >
         <div className="container">
@@ -47,19 +39,21 @@ const Header = () => {
                 {/* =========== menu start =========== */}
                 <div className='menu'>
                     <ul className='flex items-center gap-10'>
-                        <li><a className='text-white font-[600]' href="#about">About</a></li>
-                        <li><a className='text-white font-[600]' href="#services">Services</a></li>
-                        <li><a className='text-white font-[600]' href="#portfolio">Portfolio</a></li>
-                        <li><a className='text-white font-[600]' href="#contact">Contact</a></li>
+                        <li><a onClick={handleClick} className='text-white font-[600]' href="#about">About</a></li>
+                        <li><a onClick={handleClick} className='text-white font-[600]' href="#services">Services</a></li>
+                        <li><a onClick={handleClick} className='text-white font-[600]' href="#portfolio">Portfolio</a></li>
+                        <li><a onClick={handleClick} className='text-white font-[600]' href="#contact">Contact</a></li>
                     </ul>
                 </div>
                 {/* =========== menu end =========== */}
 
                 {/* =========== menu right =========== */}
                 <div className='flex items-center gap-4'>
-                    <button className='flex items-center gap-2 text-white font-[600] border border-solid border-white py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-primaryColor hover:text-white hover:font-[500] ease-in duration-300'>
-                        <i class="ri-send-plane-line"><a href="#contact"></a></i> Let's Talk
-                    </button>
+                    <a href="#contact">
+                        <button className='flex items-center gap-2 text-white font-[600] border border-solid border-white py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-primaryColor hover:text-white hover:font-[500] ease-in duration-300'>
+                            <i class="ri-send-plane-line"></i> Let's Talk
+                        </button>
+                    </a>
 
                     <span className='text-2xl text-smallTextColor md:hidden cursor-pointer'><i class="ri-menu-line"></i></span>
                 </div>
