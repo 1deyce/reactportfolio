82392044ab05519d83c7e80f4be679db6b34bc75
1deyce <124916049+1deyce@users.noreply.github.com>
2023-05-15 19:17:06 +0200

    touches

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -9,7 +9,11 @@ const Modal = ({activeID, setShowModal}) => {
             <div className="w-11/12 md:max-w-[600px] md:w-full absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform -translate-x-1/2 -translate-y-1/2 p-5">
                 <div>
                     <figure>
-                        <img className="rounded-[8px]" src={portfolio.imgUrl} alt="" />
+                        <img 
+                            className="rounded-[8px]" 
+                            src={portfolio.imgUrl} 
+                            alt="" 
+                        />
                     </figure>
                 </div>
 
