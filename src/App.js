5eeac5a147f4f67c1da9c7621816c35914e63db4
Keenan Deyce <keenandeyce@gmail.com>
2023-07-03 11:59:49 +0200

    Fixed responsive menu list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import Aos from "aos";
 import "./App.css";
+import "./components/toggle.css";
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Hero from './components/UI/Hero';
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -36,62 +36,63 @@ const Header = () => {
     const toggleMenu = ()=> menuRef.current.classList.toggle('show__menu');
 
     return (
-    <header 
-        ref={headerRef}
-        className='w-full h-[80px] leading-[80px] flex items-center bg-white dark:bg-black'
-    >
-        <div className="container">
-            <div className='flex items-center justify-between'>
-                {/* =========== logo =========== */}
-                <div className='flex items-center gap-[10px]'>
-                    <span className='w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center cursor-pointer '>
-                        KD
-                    </span>
+        <header 
+            ref={headerRef}
+            className='w-full h-[80px] leading-[80px] flex items-center bg-white dark:bg-black'
+        >
+            <div className="container">
+                <div className='flex items-center justify-between'>
+                    {/* =========== logo =========== */}
+                    <div className='flex items-center gap-[10px]'>
+                        <span className='w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center cursor-pointer '>
+                            KD
+                        </span>
 
-                    <div className='leading-[20px]'>
-                        <h2 className='text-xl text-smallTextColor dark:text-primaryColor font-[700]'>
-                            Keenan
-                        </h2>
-                        <p className='text-smallTextColor dark:text-white text-[14px] font-[500]'>
-                            Personal
-                        </p>
+                        <div className='leading-[20px]'>
+                            <h2 className='text-xl text-smallTextColor dark:text-primaryColor font-[700]'>
+                                Keenan
+                            </h2>
+                            <p className='text-smallTextColor dark:text-white text-[14px] font-[500]'>
+                                Personal
+                            </p>
+                        </div>
                     </div>
-                </div>
 
-                {/* =========== logo end =========== */}
-                {/* =========== menu start =========== */}
-                <div className='menu' ref={menuRef} onClick={toggleMenu}>
-                    <ul className='flex items-center gap-10'>
-                        <li>
-                            <a onClick={handleClick} className='text-smallTextColor dark:text-white font-[600] hover:text-primaryColor hover:dark:text-primaryColor' href="#about">About</a>
-                        </li>
-                        <li>
-                            <a onClick={handleClick} className='text-smallTextColor dark:text-white font-[600] hover:text-primaryColor hover:dark:text-primaryColor' href="#services">Services</a>
-                        </li>
-                        <li>
-                            <a onClick={handleClick} className='text-smallTextColor dark:text-white font-[600] hover:text-primaryColor hover:dark:text-primaryColor' href="#portfolio">Portfolio</a>
-                        </li>
-                        <li>
-                            <a onClick={handleClick} className='text-smallTextColor dark:text-white font-[600] hover:text-primaryColor hover:dark:text-primaryColor' href="#contact">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                {/* =========== menu end =========== */}
+                    {/* =========== logo end =========== */}
+                    {/* =========== menu start =========== */}
+                    <div className='menu' ref={menuRef} onClick={toggleMenu}>
+                        <ul className='flex items-center gap-10 dark:bg-primaryColor'>
+                            <li>
+                                <a onClick={handleClick} className='text-smallTextColor dark:text-white font-[600] hover:text-primaryColor hover:dark:text-primaryColor' href="#about">About</a>
+                            </li>
+                            <li>
+                                <a onClick={handleClick} className='text-smallTextColor dark:text-white font-[600] hover:text-primaryColor hover:dark:text-primaryColor' href="#services">Services</a>
+                            </li>
+                            <li>
+                                <a onClick={handleClick} className='text-smallTextColor dark:text-white font-[600] hover:text-primaryColor hover:dark:text-primaryColor' href="#portfolio">Portfolio</a>
+                            </li>
+                            <li>
+                                <a onClick={handleClick} className='text-smallTextColor dark:text-white font-[600] hover:text-primaryColor hover:dark:text-primaryColor' href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    {/* =========== menu end =========== */}
 
-                {/* =========== menu right =========== */}
-                <div className='flex items-center gap-4'>
-                    <a href="#contact">
-                        <button className='flex items-center gap-2 text-smallTextColor dark:text-white font-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-primaryColor dark:bg-primaryColor hover:text-white hover:dark:bg-white hover:dark:text-smallTextColor hover:font-[500] ease-in duration-300'>
-                            <i class="ri-send-plane-line"></i> Let's Talk
-                        </button>
-                    </a>
+                    {/* =========== menu right =========== */}
+                    <div className='flex items-center gap-4'>
+                        <a href="#contact">
+                            <button className='flex items-center gap-2 text-smallTextColor dark:text-white font-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-primaryColor dark:bg-primaryColor hover:text-white hover:dark:bg-white hover:dark:text-smallTextColor hover:font-[500] ease-in duration-300'
+                            >
+                                <i class="ri-send-plane-line"></i> Let's Talk
+                            </button>
+                        </a>
 
-                    <span onClick={toggleMenu} className='text-2xl text-smallTextColor md:hidden cursor-pointer'><i class="ri-menu-line"></i></span>
+                        <span onClick={toggleMenu} className='text-2xl text-smallTextColor dark:text-white md:hidden cursor-pointer'><i class="ri-menu-line"></i></span>
+                    </div>
+                    {/* =========== menu end =========== */}
                 </div>
-                {/* =========== menu end =========== */}
             </div>
-        </div>
-    </header>
+        </header>
 )};
 
 export default Header;
