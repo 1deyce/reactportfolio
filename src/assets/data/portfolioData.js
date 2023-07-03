9a160bcf68c06a4db3e498cd30e9213e504c08ef
Keenan Deyce <keenandeyce@gmail.com>
2023-07-03 13:55:48 +0200

    Update portfolioData.js

--- src/assets/data/portfolioData.js
+++ src/assets/data/portfolioData.js
@@ -14,7 +14,7 @@ const portfolios = [
     title: "Finance Technology Website",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    technologies: ["React.js", "Tailwind css", "Node.js", "MongoDB"],
     siteUrl: "#",
   },
   {
