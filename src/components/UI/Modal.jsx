522d060b2cf7a1f99709a41b5fa1690162aba684
Keenan Deyce <keenandeyce@gmail.com>
2023-07-03 13:49:05 +0200

    Update Modal.jsx
    Added the CSS styles maxHeight: '80vh' and overflowY: 'auto' to the modal content div (w-11/12 md:max-w-[600px] md:w-full bg-white dark:bg-black rounded-[8px] p-5)

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -28,13 +28,14 @@ const Modal = ({activeID, setShowModal}) => {
             <div 
                 className="w-11/12 md:max-w-[600px] md:w-full absolute top-1/2 left-1/2 z-20 bg-white dark:bg-black rounded-[8px] transform -translate-x-1/2 -translate-y-1/2 p-5"
                 onClick={handleModalScroll}
+                style={{ maxHeight: '80vh', overflowY: 'auto' }}
             >
                 <div>
                     <figure>
                         <img 
                             className="rounded-[8px]" 
                             src={portfolio.imgUrl} 
-                            alt="" 
+                            alt=""
                         />
                     </figure>
                 </div>
